d1f7efebe846c563be5dd72c058c942140904b92
Florian Witteler <(none)>
2016-04-08 14:57:38 +0200

    Parent- with three child- components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,59 @@
+var USER_DATA = {
+  name: 'Florian Witteler',
+  username: 'flowi',
+  imageUrl: 'https://avatars0.githubusercontent.com/u/39078?v=3&s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+/* all react components must be F.I.R.S.T.
+  Focused
+  Independent
+  Reusable
+  Small
+  Testable
+*/
+
+var ProfilePic = React.createClass({
+  render: function() {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function() {
     return (
-      <div> Hello {this.props.name} </div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username} >
+          {this.props.username}
+        </a>
+      </div>
     )
   }
 });
 
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div> {this.props.name} </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.imageUrl} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWorld name="Florian" />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
